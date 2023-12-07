a5790f24d335f839add8a13f672a538e9dc6aaa1
Nn0el <noel.johansson@elev.ga.ntig.se>
2023-12-07 13:47:58 +0100

    klar

---
+++ Franklin.js
@@ -0,0 +1,9 @@
+import Pingvin from "./Pingvin";
+
+export default class extends Pingvin{
+namn= 'Franklin'
+beak = 'big beak'
+vikt = 10
+length = 110
+
+}
---
+++ Pingvin.js
@@ -0,0 +1,8 @@
+export default class Pingvin{
+    constructor(Pingvin){
+this.namn = namn
+this.beak = beak
+this.vikt = vikt
+this.length = length
+}
+}
